ac9036209b7887415ba29f218c1fa5f22caaafec
LeoDomGit <leodomsolar@gmail.com>
2024-05-14 21:30:48 +0700

    Commit

--- packages/users/routes/web.php
+++ packages/users/routes/web.php
@@ -4,6 +4,10 @@ use Leo\Users\Controllers\UserController;
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::prefix('users')->name('users.')->group(function () {
-    Route::resource('/', UserController::class);
+    Route::get('/', [UserController::class, 'index'])->name('index');
+    Route::get('/create', [UserController::class, 'create'])->name('create');
+    Route::post('/', [UserController::class, 'store'])->name('store');
+    Route::get('/{user}', [UserController::class, 'show'])->name('show');
 });
