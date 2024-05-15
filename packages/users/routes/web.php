882d4d8122e18ce8f2a81eaa679b5bd8aeb0b82b
LeoDomGit <leodomsolar@gmail.com>
2024-05-15 10:14:15 +0700

    Commit

--- packages/users/routes/web.php
+++ packages/users/routes/web.php
@@ -9,7 +9,5 @@ Route::prefix('api/users')->name('api.users.')->group(function () {
     Route::get('/', [UserController::class, 'index'])->name('index');
     Route::get('/create', [UserController::class, 'create'])->name('create');
     Route::post('/', [UserController::class, 'store'])->name('store');
-    Route::post('/checkLogin', [UserController::class, 'store'])->name('checkLogin');
-    Route::post('/checkLogin-social', [UserController::class, 'store'])->name('checkLoginSocial');
     Route::get('/{user}', [UserController::class, 'show'])->name('show');
 });
